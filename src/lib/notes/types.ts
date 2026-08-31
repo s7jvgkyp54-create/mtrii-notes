46ef6bfce328f43c0d7ce1fceed6fd01973c773e
Mtrii <mtrii@local>
2026-08-31 22:15:46 +0700

    Release Notes v0.3.22

--- src/lib/notes/types.ts
+++ src/lib/notes/types.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "Notes";
 export const APP_ID = "com.mtrii.notes";
-export const APP_VERSION = "0.3.13";
+export const APP_VERSION = "0.3.22";
 export const SCHEMA_VERSION = 1;
 export const BACKUP_FORMAT = "notesbackup";
 export const BACKUP_FORMAT_VERSION = 1;
